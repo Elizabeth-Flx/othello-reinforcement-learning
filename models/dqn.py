b5b41576224484dbe9071d46d0ed0a5f0bbf08a0
Elizabeth Lochert <daniel.lochert.flx@gmail.com>
2025-03-03 17:38:05 +0100

    implemented simple TTTDQN

--- models/dqn.py
+++ models/dqn.py
@@ -15,17 +15,34 @@ class OthelloDQN(nn.Module):
         self.fc1 = nn.Linear(64*8*8, 512)
         self.fc2 = nn.Linear(512, 64)
 
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid() 
-
     def forward(self, x):
-        x = self.relu(self.conv3x3(x))
-        x = self.relu(self.conv5x5(x))
+        x = torch.relu(self.conv3x3(x))
+        x = torch.relu(self.conv5x5(x))
 
         x = x.view(x.size(0), -1) # Flatten
 
-        x = self.relu(self.fc1(x))
+        x = torch.relu(self.fc1(x))
         x = self.fc2(x)
-        x = self.sigmoid(x)
+        x = torch.sigmoid(x)
 
         return x
+    
+class TTTDQN(nn.Module):
+
+    def __init__(self):
+        super(TTTDQN, self).__init__()
+
+        self.fc1 = nn.Linear(9, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 9)
+
+    def forward(self, x):
+        x = torch.tensor(x).flatten()
+
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+
+        return x
+
+
